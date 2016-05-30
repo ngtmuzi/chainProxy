238d0ae7bc68c38f29fefd5de81f6a61d9005e53
ngtmuzi <ngtmuzi@qq.com>
2016-05-30 15:21:20 +0800

    support default value

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ this module only can run in NodeJs6 because it use `Proxy`
 you can use it like:
 
 ```javascript
-var _ = require('chainproxy')
+var _ = require('chainproxy')();
 
 Promise.resolve({a: 12333})
   .then(_.a.toString().split('')[0].toString().replace('1', 'replaceStr').length)
   .then(console.log.bind(null, 'result:'), console.error.bind(null, 'catch error:'));
 ```
+you can set default value in `require('chainproxy')();` param, if not, will throw an error, see test example.
 
 this module actually create function like that
 
@@ -26,4 +27,4 @@ function (args, _){
   _.a.toString().split(args[0])[args[1]].toString().replace(args[2], args[3]).length
 }
 ```
-so you can know it's vary quick and easy-use
+so you can see it's vary quick and easy-use
--- index.js
+++ index.js
@@ -2,47 +2,63 @@
  * Created by ngtmuzi on 2016/5/29.
  */
 'use strict';
-var receiveFn = (...args) => args;
-
-var handle = {
-  get: function (target, property, receiver) {
-    //set prototype to base Proxy Object _
-    if (property === 'prototype') return _;
-
-    //is number
-    if (!isNaN(+property)) return new Proxy(target.bind(null, `[${property}]`), handle);
-    //return a new Proxy, go on
-    return new Proxy(target.bind(null, '.' + property), handle);
-  },
-
-  apply: function (target, thisArg, argumentsList) {
-    //if method calling on Proxy Object
-    if (thisArg && thisArg.prototype === _) {
-      //save arguments to chain, and go on
-      return new Proxy(target.bind(null, argumentsList), handle);
-
-    } else {  //calling on outside
-
-      //get the calling chain
-      var chains = target();
-      //pick arguments
-      var args = [].concat(...chains.filter(Array.isArray));
-
-      //make function body
-      var argNum = 0;
-      var fnStr = chains.reduce(function (a, b) {
-        if (typeof b === 'string') return a + b;
-        else if (Array.isArray(b)) return a + `(${b.map(()=> `args[${argNum++}]`)})`;
-      }, 'return _');
-
-      var finalFn = new Function(['args', '_'], fnStr);
-
-      return finalFn(args, argumentsList[0]);
-    }
-  }
-};
+const receiveFn = (...args) => args;
+const voidArgs  = Symbol('voidArgs');
 
 /**
+ * chain proxy maker
+ * @params {Any} defaultValue
  * @type {Function}
  */
-var _ = module.exports = new Proxy(receiveFn, handle);
+const chainProxy = module.exports = function (defaultValue) {
+  var hasDefault   = arguments.length > 0;
+
+  var handle = {
+    get: function (target, property, receiver) {
+      //set prototype to base Proxy Object _
+      if (property === 'prototype') return _;
+
+      //is number
+      if (!isNaN(+property)) return new Proxy(target.bind(null, `[${property}]`), handle);
+      //return a new Proxy, go on
+      return new Proxy(target.bind(null, '.' + property), handle);
+    },
+
+    apply: function (target, thisArg, argumentsList) {
+      //if method calling on Proxy Object
+      if (thisArg && thisArg.prototype === _) {
+        //save arguments to chain, and go on
+        return new Proxy(target.bind(null, argumentsList), handle);
+
+      } else {  //calling on outside
+
+        //get the calling chain
+        var chains = target();
+        //pick arguments
+        var args   = [].concat(...chains.filter(Array.isArray));
+
+        //make function body
+        var argNum     = 0;
+        var expression = chains.reduce(function (a, b) {
+          if (typeof b === 'string') return a + b;
+          else if (Array.isArray(b)) return a + `(${b.map(()=> `args[${argNum++}]`)})`;
+        }, 'return _');
+
+        var fnStr = `
+        try{
+          ${expression};
+        }catch(err){
+          ${hasDefault ? 'return defaultValue;' : ''}
+          err.stack = err.name + ': ' + err.message + '\\n\\tat: ' + '${expression}';
+          throw err;
+        }`;
+
+        var finalFn = new Function(['args', '_', 'defaultValue'], fnStr);
+        return finalFn(args, argumentsList[0], defaultValue);
+      }
+    }
+  };
+
+  var _ = new Proxy(receiveFn, handle);
+  return _;
+};
